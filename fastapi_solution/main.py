d3f0ed38c531d8801a0abaf08718e3872e5f3b44
Alena Markina <markina.a7@yandex.ru>
2024-07-25 21:03:30 +0200

    Add base project structure

---
+++ fastapi_solution/main.py
@@ -0,0 +1,31 @@
+from src.api.v1 import films
+from src.core import config
+from src.db import elastic, redis
+
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from redis.asyncio import Redis
+from elasticsearch import AsyncElasticsearch
+
+
+app = FastAPI(
+    title=config.PROJECT_NAME,
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
+    default_response_class=ORJSONResponse,
+)
+
+
+@app.on_event('startup')
+async def startup():
+    redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+    elastic.es = AsyncElasticsearch(hosts=[f'http://{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    await redis.redis.close()
+    await elastic.es.close()
+
+
+app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
